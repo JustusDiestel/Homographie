8715dc6f535d08c5a88e8f00a6b50ed4dbb4f89e
justusdiestel <justusdiestel@web.de>
2026-04-01 13:32:08 +0800

    bilder ploppen noch nicht auf.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import cv2
 from pointer import get_points
 
 
@@ -5,10 +6,21 @@ def main():
     pts_img1 = get_points("1.jpg", "Bild 1")
     pts_img2 = get_points("2.jpg", "Bild 2")
 
+    # OPTIONAL: nur anzeigen wenn Datei existiert
+    img1 = cv2.imread("Bild1_pts.png")
+    img2 = cv2.imread("Bild2_pts.png")
+
+    if img1 is not None:
+        cv2.imshow("Bild 1", img1)
+
+    if img2 is not None:
+        cv2.imshow("Bild 2", img2)
+
     print("Bild1:", pts_img1)
     print("Bild2:", pts_img2)
 
-
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
--- pointer.py
+++ pointer.py
@@ -42,6 +42,7 @@ def get_points(image_path, window_name):
         elif key == 27:
             break
 
-    cv2.destroyWindow(window_name)
+    cv2.imwrite(f'{window_name}_pts.png', clone)
+    cv2.destroyAllWindows()
     return points
 
